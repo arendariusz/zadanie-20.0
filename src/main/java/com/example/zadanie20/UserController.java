f5de0356fbed148dab6bbebc16a9e2b381a334fb
arendariusz <d.arendarski92@gmail.com>
2022-02-20 16:34:19 +0100

    Zadanie 20.0 rozwiazanie poprawka 1

--- src/main/java/com/example/zadanie20/UserController.java
+++ src/main/java/com/example/zadanie20/UserController.java
@@ -1,9 +1,7 @@
 package com.example.zadanie20;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,7 +14,7 @@ public class UserController {
     }
 
     @ResponseBody
-    @RequestMapping("/users")
+    @GetMapping("/users")
     public String list() {
         List<User> userList = userRepository.getAll();
         String result = "";
@@ -26,21 +24,9 @@ public class UserController {
         return result;
     }
 
-    @RequestMapping("/add")
-    public String addFromParam(@RequestParam(name = "imie") String name, @RequestParam(name = "nazwisko") String lastName,
-                               @RequestParam(name = "wiek") Integer age) {
-        if (isNameCorrect(name)) {
-            User user = new User(name, lastName, age);
-            userRepository.add(user);
-            return "success.html";
-        }
-        return "err.html";
-    }
-
-    @RequestMapping("/addForm")
-    public String addFromForm(@RequestParam String name,
-                              @RequestParam String lastName,
-                              @RequestParam Integer age) {
+    @RequestMapping(path = "add", method = {RequestMethod.GET, RequestMethod.POST})
+    public String addUser(@RequestParam(name = "imie") String name, @RequestParam(name = "nazwisko") String lastName,
+                              @RequestParam(name = "wiek") Integer age) {
         if (!isNameCorrect(name)) {
             return "redirect:/err.html";
         }
@@ -50,9 +36,6 @@ public class UserController {
     }
 
     private boolean isNameCorrect(String name) {
-        if (name == null || name.length() <= 1) {
-            return false;
-        }
-        return true;
+        return name != null && name.length() > 1;
     }
 }
